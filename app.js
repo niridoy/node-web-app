1d86c102d4069a6e3c56d9e29f6d4a28cd262a60
Nazmul Islam <nazmul.islam.ridoy@gmail.com>
2025-10-02 15:56:00 +0600

    Update welcome message content

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const exapp.get('/', (req, res) => {
-  res.send('Welcome to our Node.js Application - Deployed with ArgoCD and GitHub Actions!\n');ess = require('express');
+    res.send('Welcome to our Node.js Application - Deployed with ArgoCD and GitHub Actions!\n'); ess = require('express');
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+    // Constants
+    const PORT = 8080;
+    const HOST = '0.0.0.0';
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello World from Node.js Web App!\n');
-});
+    // App
+    const app = express();
+    app.get('/', (req, res) => {
+        res.send('Hello World from Node.js Web App!\n');
+    });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+    app.listen(PORT, HOST);
+    console.log(`Running on http://${HOST}:${PORT}`);
