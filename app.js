53307e321c335ef95cc20818b138d5b17c62cd68
Nazmul Islam <nazmul.islam.ridoy@gmail.com>
2025-10-02 15:27:50 +0600

    Add CI/CD configuration with GitHub Actions and ArgoCD

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World from Node.js Web App!\n');
+    res.send('Hello World from Node.js Web App!\n');
 });
 
 app.listen(PORT, HOST);
